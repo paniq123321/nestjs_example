9ad780cc028ad7a1c34177a27de0f40777fa1e35
paniq123321 <ecepaniq@gmail.com>
2025-06-19 12:10:05 -0300

    upd

--- src/flowers/flowers.module.ts
+++ src/flowers/flowers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { FlowersService } from './flowers.service';
 import { FlowersController } from './flowers.controller';
 
 @Module({
   controllers: [FlowersController],
-  providers: [FlowersService],
+  providers: [FlowersService, PrismaService],
 })
 export class FlowersModule {}
--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Flower } from 'generated/prisma';
+import { PrismaService } from '../prisma.service';
+
+
 
 @Injectable()
 export class FlowersService {
-  findAll() {
-    return [{
-      name : 'Rose',
-      color: 'red',
-      price: 5
-    },
-      {
-        name : 'Lily',
-        color: 'white',
-        price: 10
-      },
-      {
-        name : 'Tulip',
-        color: 'yellow',
-        price: 7
-      }
-    ];
+  constructor(private readonly prisma:PrismaService){
+
+  }
+  findAll(){
+    return this.prisma.flower.findMany()
   }
 }
+
+// return [{
+//   name : 'Rose',
+//   color: 'red',
+//   price: 5
+// },
+//   {
+//     name : 'Lily',
+//     color: 'white',
+//     price: 10
+//   },
+//   {
+//     name : 'Tulip',
+//     color: 'yellow',
+//     price: 7
+//   }
+// ]
